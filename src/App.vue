7ca5fce28ca4fe151ee4077bed1aecabfef07bce
Eoin Pinaqui <pinaquie@tcd.ie>
2023-07-23 15:00:25 +0100

    Colour change + finesse

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,30 @@
         <v-card id="content">
           <v-row>
             <v-col>
-              <div class="greeting">
-                <h2>Hi there! I'm</h2>
-                <h1>Eoin Pinaqui</h1>
-              </div>
+              <h2>Hi there! I'm</h2>
+              <h1>Eoin Pinaqui</h1>
             </v-col>
           </v-row>
+          <div class="space"></div>
           <v-row>
             <v-col>
-              <div class="button">
-                <p>I am a passionate software engineer with a relentless drive for technology, problem-solving, and innovation.</p>
-                <v-btn
-                    style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"
-                    color="#008BFF"
-                    @click.prevent="openResume"
-                >
-                  See my Resume
-                </v-btn>
-              </div>
+              <p>I am a passionate software engineer with a relentless drive for technology, problem-solving, and innovation.</p>
+              <v-btn
+                  class="shadow"
+                  color="#87CEFA"
+                  @click.prevent="openResume"
+              >
+                See my Resume
+              </v-btn>
             </v-col>
           </v-row>
+          <div class="space"></div>
           <v-row>
             <v-col>
-              <div class="image_div_circle">
-                <v-img
-                    :src="require('./assets/selfie.jpg')"
-                    class="image_circle"
-                ></v-img>
-              </div>
+              <v-img
+                  :src="require('./assets/selfie.jpg')"
+                  class="image shadow"
+              ></v-img>
             </v-col>
           </v-row>
         </v-card>
@@ -54,48 +50,42 @@ export default {
 
 #app {
   font-family: "Roboto", sans-serif;
-  background-color: #008BFF;
+  background-color: #87CEFA;
 }
 
 #content {
-  margin: 5em 0;
+  margin: 10vh 5vw;
+  padding: 10vh 5vw;
+  text-align: center;
 }
 
-.greeting {
-  text-align: center;
-  margin: 5em 0 0 0;
+.space {
+  margin: 3em;
+}
+
+.shadow {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.image {
+  border-radius: 50%;
+  width: 50%;
+  min-width: 250px;
+  margin: 0 auto;
 }
 
 h1 {
-  font-size: 3.5em !important;
-  font-weight: 900 !important;
+  font-size: calc(3.5em + 0.5vw);
+  font-weight: 900;
 }
 
 h2 {
-  font-size: 2em !important;
-  font-weight: 300 !important;
-}
-
-.image_div_circle {
-  width: 100%;
-  max-width: 400px;
-  text-align: center;
-  margin: 0em auto 5em auto;
-}
-
-.image_circle {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  border-radius: 50%;
+  font-size: calc(2em + 0.5vw);
+  font-weight: 300;
 }
 
 p {
-  font-size: 1.25em !important;
-  font-weight: 300 !important;
+  font-size: calc(1em + 0.5vw);
+  font-weight: 300;
 }
-
-.button {
-  text-align: center;
-  margin: 2em 0 ;
-}
-
 </style>
